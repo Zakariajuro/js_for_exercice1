edfb05c7685989b5c97af948d0e22067f233ac27
Zakaria <zakPro@live.be>
2021-01-07 16:37:13 +0100

    exo-fini

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// EXERCICE 1
+
+// for (let i = 0; i < 20 ; i++) {
+//     if (i % 2 == 0) {
+//         console.log(`${i} est un nombre paire`);
+//     } else {
+//         console.log(`${i} n'est pas un nombre paire`);
+//     }
+//     // console.log(i);
+// }
+
+// EXERCICE 2
+
+// let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"];
+
+// for (let i = 0; i < coding16.length; i++) {
+//     console.log(`bonjour, ${coding16[i]}`);
+    
+// }
+
+
+// EXERCICE 3
+
+// let nombre = prompt("indiquez un nombre");
+
+// for (let i = 0; i < nombre; i++) {
+//     console.log(i);    
+// }
